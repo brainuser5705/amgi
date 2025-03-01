67a2f2353156a344811610fc35a721210c57e901
brainuser5705 <codeuser5705@outlook.com>
2025-03-01 16:35:22 -0500

    add: anki connect basic requests

---
+++ scripts/anki_connect.js
@@ -0,0 +1,68 @@
+const ANKI_CONNECT_URL = "http://127.0.0.1:8765"
+
+/**
+ * Creates a basic Request promise to Anki-Connect
+ * running on localhost
+ * 
+ * @param {*} action    the Anki-Connect action
+ * @param {*} version   the version of the action
+ * @param {*} params    additional parameters of the request
+ * @returns Request Promise
+ */
+function requestToAnki(action, version, params={}) {
+
+    let request = new Request(ANKI_CONNECT_URL, {
+        method: "POST",
+        body: JSON.stringify({action, version, params})
+    });
+
+    return fetch(request);
+
+};
+
+function guiAddRequest(selection){
+
+    requestToAnki("guiAddCards", 6, {
+        "note": {
+            "deckName": "Default",
+            "modelName": "Basic",
+            "fields": {
+                "Front": selection,
+                "Back": "the meaning of " + selection
+            }
+        }
+    })
+    .then((response) => {
+        if (response.status == 200) {
+            console.log(response.status);
+        }else{
+            console.log("User pressed no?");
+        }
+    });
+
+}
+
+function addNoteRequest(selection){
+    requestToAnki("addNote", 6, {
+        "note": {
+            "deckName": "Default",
+            "modelName": "Basic",
+            "fields": {
+                "Front": selection,
+                "Back": "definition of" + selection
+            },
+            "options": {
+                "allowDuplicate": false,
+            }
+        }
+    })
+    .then((response) => {
+        if (response.status == 200) {
+            console.log("Added a card");
+        }else{
+            console.log("User pressed no?");
+        }
+    });
+}
+
+export { guiAddRequest, addNoteRequest };
