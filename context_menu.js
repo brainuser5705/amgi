d93ca0785bee37e54b97b3b8dba7f7ba869718d1
brainuser5705 <codeuser5705@outlook.com>
2025-03-02 01:00:22 -0500

    i hope this works

--- context_menu.js
+++ context_menu.js
@@ -1,3 +1,5 @@
+import { guiAddRequest, addNoteRequest } from "./scripts/anki_connect.js";
+
 // When the service worker starts running, it adds the context
 // menu when the user selects it.
 chrome.runtime.onInstalled.addListener(function () {
@@ -8,3 +10,18 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 
 });
+
+async function genericCallback(info, tab) {
+
+    console.log("fetching...");
+
+    // requestToAnki("deckNames", 6)
+    // .then((response) => response.json()) // promise that returns JSON value from response body
+    // .then((data) => console.log(data));  // gets the result
+
+    // guiAddRequest(info.selectionText);
+    addNoteRequest(info.selectionText);
+
+}
+
+chrome.contextMenus.onClicked.addListener(genericCallback);
